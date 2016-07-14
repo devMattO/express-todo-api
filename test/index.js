c51b49dd0f083e2c6bac820b5d1cf73748585cff
Mattdre <olsen_matthew@yahoo.com>
2016-07-14 09:43:16 -1000

    Writing tests for different status codes

--- test/index.js
+++ test/index.js
@@ -24,6 +24,17 @@ describe('/POST', function () {
       .expect({'success': true})
       .expect(200, done);
   });
+  it('should return a status code 400 if the request cannot be filled due to bad syntax', function(done){
+    supertest(app)
+      .post('/buzzword')
+      .send({"buzzward":"shootz", "pints": 2})
+      .expect(400, done);
+  });
+  it('should return a status code 404 if the request page cannot be found', function(done){
+    supertest(app)
+      .post('/buzzward')
+      .expect(404, done);
+  });
   it('should respond with a success-true or false', function (done) {
     supertest(app)
       .post('/buzzword')
@@ -37,6 +48,11 @@ describe('/PUT', function () {
     supertest(app)
       .put('/buzzword')
       .expect(200, done);
+  });
+  it('should return a status code 404 if the request page cannot be found', function(done){
+    supertest(app)
+      .put('/buzzward')
+      .expect(404, done);
   });
   it('should respond with success: true/false and newScore: Number', function (done) {
     supertest(app)
